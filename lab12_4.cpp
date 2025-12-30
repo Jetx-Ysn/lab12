0edddce3a73043b414770d8f92cb93dcfd8f8578
Jet <Yotsanon_aim@cmu.ac.th>
2025-12-30 15:38:24 +0700

    lab_12_4.cpp completed

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename T>
+void mySwap(T &,T &);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -28,5 +31,13 @@ int main(){
 	cout << "p = " << p << ", q = " << q << "\n";
 
 	return 0;
+}
+
+template <typename R>
+void mySwap(R &x,R &y){
+	R i;
+	 i = x;
+	 x = y;
+	 y = i;
 }
 
